3c51d2e7724686b1a41b20a379b68b4e829c2ed1
Raji Kaipu <rl.kaipu@gmail.com>
2023-01-17 10:32:25 +0000

    add javascript

---
+++ Searchbar/search.js
@@ -0,0 +1,13 @@
+windows.onload = function(){
+function myFunction(){
+var input, filter, ul, li, a, i;
+input = document.getElementById("mySearch");
+filter = input.value.toUpperCase();
+ul = document.getElementById("myMenu");
+li = ul.getElementsByTagName("li");
+for(i=0; i<li.length; i++){
+    a = li[i].getElementsByTagName("a")[0];
+}
+}
+}
+
